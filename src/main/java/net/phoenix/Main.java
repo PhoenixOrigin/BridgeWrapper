d39c244dc2d06b5de286c9d5814caea9d0a8038b
nHexanol <nHexanol@gmail.com>
2023-06-17 17:18:00 +0100

    test

--- src/main/java/net/phoenix/Main.java
+++ src/main/java/net/phoenix/Main.java
@@ -1,7 +1,9 @@
 package net.phoenix;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Process p = new ProcessBuilder("").start();
     }
 }
